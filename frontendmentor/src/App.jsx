a4bc41170c9be943f5a2c2ea58e589903035d990
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-06-19 13:31:58 +0300

    correction du chemin qui importe le QrCode dans App.jsx

--- frontendmentor/src/App.jsx
+++ frontendmentor/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/home/HomePage";
-import QrCode from "./components/projet-1/qr-code-component-main/Qrcode";
+import QrCode from "./components/projet-1/qr-code-component-main/QrCode";
 
 function App() {
     return (
