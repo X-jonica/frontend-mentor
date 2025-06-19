4a5d5b58c025121f181fcd2dd6b43c6da13dcf18
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-06-19 13:04:48 +0300

    re-installation de gh-pages

--- frontendmentor/src/components/layout/Header.jsx
+++ frontendmentor/src/components/layout/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
-  return (
-      <header style={{ padding: "20px", backgroundColor: "#f5f5f5" }}>
-          
-      </header>
-  );
+    return (
+        <header
+            style={{ padding: "20px", backgroundColor: "#f5f5f5" }}
+        ></header>
+    );
 }
 
 export default Header;
